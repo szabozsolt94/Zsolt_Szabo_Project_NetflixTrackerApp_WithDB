8e54bd26aaa9d413ae2fcb73fb80f82c19869f18
szabozsolt94 <zsolt_taylor@yahoo.com>
2025-04-26 16:56:24 +0100

    FIxed some bugs and CSS issues.

---
+++ Zsolt_Szabo_Project_NetflixTrackerApp/wwwroot/js/Home.js
@@ -0,0 +1,37 @@
+﻿// Fetch all the Netflix movies from the API
+$(document).ready(function () {
+
+    // Set the API endpoint URL
+    var apiUrl = "http://localhost:62481/api/NetflixMovies";
+
+    // Make a GET request to the API
+    $.ajax({
+        url: apiUrl,
+        type: "GET",
+
+        // If successful, loop through the movies and create cards
+        success: function (movies) {
+            movies.forEach(function (movie) {
+                var $card = $('<div>', { class: 'card' });
+                var $title = $('<h3>').text(`${movie.Title} (${movie.Year})`);
+                var $img = $('<img>', {
+                    src: `data:image/jpeg;base64,${movie.Image}`,
+                    alt: movie.Title
+                });
+
+                // When the image has fully loaded, add the 'loaded' class to trigger CSS effects (this was needed due to issue with CSS not applying to images)
+                $img.on('load', function () {
+                    $(this).addClass('loaded');
+                });
+
+                $card.append($title).append($img);
+                $('#moviesGrid').append($card);
+            });
+        },
+
+        // If the request fails
+        error: function () {
+            alert("Error fetching movie data.");
+        }
+    });
+});
